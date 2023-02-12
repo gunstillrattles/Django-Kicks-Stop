265ba8b71639ed5da07632dd9bc49a53958d25ba
gunstillrattles <marlen.mukashev@narxoz.kz>
2023-02-12 17:30:32 +0600

    Adding models

--- sneakers/models.py
+++ sneakers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField()
+    image_name = models.CharField(max_length=100)
