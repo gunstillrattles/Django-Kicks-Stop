a49befa8667ffbd63ff3149d23576228cbf21ac8
gunstillrattles <marlen.mukashev@narxoz.kz>
2023-03-09 03:43:34 +0600

    Lab 3,4,5

--- sneakers/admin.py
+++ sneakers/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import *
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'size', 'description')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'category')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'time_create')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
 
 admin.site.register(Category)
 admin.site.register(Product)
--- sneakers/models.py
+++ sneakers/models.py
@@ -1,12 +1,17 @@
 from django.db import models
-
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="Категория")
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
+
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    size = models.DecimalField(max_digits=5, decimal_places=2)
-    description = models.TextField()
-    image_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="Название кроссовок")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    size = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Размер обуви")
+    description = models.TextField(max_length=366, verbose_name="Описание")
+    image_name = models.CharField(max_length=100, verbose_name="Название картинки")
