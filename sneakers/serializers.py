381c0682299448ab01cd62c53b85ee7d5cc57190
gunstillrattles <marlen.mukashev@narxoz.kz>
2023-04-26 20:50:17 +0600

    Permissions and serialization didn't add somehow, nice IDE

---
+++ sneakers/serializers.py
@@ -0,0 +1,16 @@
+import io
+
+from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
+from sneakers.models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Product
+        fields = ("__all__")
+
